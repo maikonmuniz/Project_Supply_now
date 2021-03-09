3c8060171350f1d3c08f9ef478153a596a68fb39
João Alves <jvamarangonia@gmail.com>
2021-03-09 20:41:57 -0300

    1

--- supply_now/supply_now/settings.py
+++ supply_now/supply_now/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ.get('DB_NAME', 'supply_now'),
             'USER': os.environ.get('DB_USER', 'postgres'),
-            'PASSWORD': os.environ.get('DB_PASS', 'admin123'),
+            'PASSWORD': os.environ.get('DB_PASS', ''),
             'HOST': 'localhost',
             'PORT': '5432',
         }
